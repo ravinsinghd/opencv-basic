f811278dbfb2ff97baa680b42f330607cbcdabe4
Ravin Singh D <ravinsinghd@outlook.com>
2016-03-18 07:48:30 +0530

    tesseract output improved after adaptive threshold

--- adoptivethreshold.py
+++ adoptivethreshold.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('google.png',0)
+img = cv2.imread('text.png',0)
 #img = cv2.medianBlur(img,5)
 
 ret,th1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
@@ -10,7 +10,7 @@ th2 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
             cv2.THRESH_BINARY,11,2)
 th3 = cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv2.THRESH_BINARY,11,2)
-
+cv2.imwrite('adoThres.png',th3);
 titles = ['Original Image', 'Global Thresholding (v = 127)',
             'Adaptive Mean Thresholding', 'Adaptive Gaussian Thresholding']
 images = [img, th1, th2, th3]
--- tesseract.py
+++ tesseract.py
@@ -1,6 +1,13 @@
-from PIL import Image
-import pytesseract
-img=Image.open(r'test.png');
-print(img)
-print(pytesseract.image_to_string(img))
+import subprocess
+test = subprocess.Popen(["tesseract","text.png","out"], stdout=subprocess.PIPE)
+test.communicate()[0]
+f = open("out.txt")
+print('Normal Image')
+print(f.read().strip())
+print('processed image')
+test = subprocess.Popen(["tesseract","adoThres.png","out"], stdout=subprocess.PIPE)
+test.communicate()[0]
+f = open("out.txt")
+print(f.read().strip())
+
 
